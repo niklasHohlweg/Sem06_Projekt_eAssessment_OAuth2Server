dc2d2ede044046e24032325e19244c5699599c8e
niklasHohlweg <hohlweg.niklas@gmail.com>
2024-03-27 13:11:31 +0100

    init

---
+++ src/main/java/com/oauth2/eassessment/EassessmentApplication.java
@@ -0,0 +1,13 @@
+package com.oauth2.eassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EassessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EassessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oauth2/eassessment/EassessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth2.eassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EassessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
